a753465abe6943ec811e4f79a022e587051aeeaa
Kane Hall <kanehall45@hotmail.co.uk>
2023-08-07 21:26:14 +0100

    added images, routes

---
+++ portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,32 @@
+import './index.scss'
+import {Link, NavLink } from 'react-router-dom'
+import K_logo from '../../assets/images/k-logo.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => (
+    <div className="nav-bar">
+        <Link className="logo" to="/">
+            <img src={K_logo} alt="logo"/>
+            <img className="sub-logo" src={LogoSubtitle} alt="kanehall"/>
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="contact-link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+
+
+        </nav>
+    </div>
+)
+
+export default Sidebar;
