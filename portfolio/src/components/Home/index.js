1c7636462af84b9a686b0f666a5500aaa6d509f4
Kane Hall <kanehall45@hotmail.co.uk>
2023-08-12 21:40:28 +0100

    problems solved!

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,43 +1,40 @@
-// import LogoTitle from '../../assets/images/k-logo.png';
-// import { Link } from 'react-router-dom';
-// import './index.scss'
-
-
-
-// const Home = () => {
-//   return (
-//     <div className="container home-page">
-//       <div className="text-zone">
-//         <h1> Hi, <br/> I'm
-//         <img src={LogoTitle} alt="developer" />
-//         ane
-//         <br/>
-//         Junior Software Developer
-//         </h1>
-//         <h2> Fully trained junior full-stack developer / ex Data Analyst</h2>
-//         <Link to='/contact' className='flat-button'> Contact Me </Link>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
 import LogoTitle from '../../assets/images/k-logo.png';
 import { Link } from 'react-router-dom';
-import './index.scss';
+import {useState} from 'react';
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+
+
 
 const Home = () => {
+
+const[letterClass, setLetterClass] = useState('text-animate');
+const nameArray = ['a', 'n', 'e','.'];
+const jobArray = ['J', 'u', 'n','i','o','r',' ','S','o', 'f','t','w','a','r','e', ' ', 'D','e','v','e','l','o','p','e','r','.']
+
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1> Hi, <br/> I'm
-          <img src={LogoTitle} alt="developer" height="100" width="100" />
-          ane
-          <br/>
-          Junior Software Developer
+      
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          
+         <br/>
+         <span className={`${letterClass} _13`}>I</span>
+         <span className={`${letterClass} _14`}>'m</span>
+
+        <img src={LogoTitle} alt="developer" />
+        <AnimatedLetters letterClass={letterClass} 
+        strArray={nameArray} 
+        idx={15} />
+        <br/>
+        <AnimatedLetters letterClass={letterClass} 
+        strArray={jobArray} 
+        idx={10} />
         </h1>
         <h2> Fully trained junior full-stack developer / ex Data Analyst</h2>
-        <Link to='/contact' className='flat-button' activeClassName='active'> Contact Me </Link>
+        <Link to='/contact' className='flat-button'> Contact Me </Link>
       </div>
     </div>
   );
--- portfolio/src/components/Layout/index.js
+++ portfolio/src/components/Layout/index.js
@@ -1,30 +1,7 @@
-// import { Outlet } from 'react-router-dom'
-// import Sidebar from '../Sidebar/'
-// import './index.scss'
 
-// const Layout = () => {
-//   return (
-//     <div className="App">
-//       <Sidebar />
-//       <div className="page">
-//         <span className="tags top-tags">&lt;body&gt;</span>
-        
-//         <Outlet />
-        
-//         <span className="tags bottom-tags">
-//           &lt;/body&gt;
-//           <br />
-//           <span className="bottom-tag-html">&lt;/html&gt;</span>
-//         </span>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Layout
 
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../Sidebar/'
+import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
@@ -35,7 +12,6 @@ const Layout = () => {
         <span className="tags top-tags">&lt;body&gt;</span>
 
         <Outlet />
-        
         <span className="tags bottom-tags">
           &lt;/body&gt;
           <br />
